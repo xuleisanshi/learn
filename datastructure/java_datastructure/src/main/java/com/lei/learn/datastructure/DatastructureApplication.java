e862b3d33ab112399dced63faf7d3ffc2afac119
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-04-24 20:28:15 +0800

    [init]rest project

--- datastructure/java_datastructure/src/main/java/com/lei/learn/datastructure/DatastructureApplication.java
+++ datastructure/java_datastructure/src/main/java/com/lei/learn/datastructure/DatastructureApplication.java
@@ -2,12 +2,22 @@ package com.lei.learn.datastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class DatastructureApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatastructureApplication.class, args);
+	@GetMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Hello World!";
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructureApplication.class, args); //启动项目
+	}
 }
+
