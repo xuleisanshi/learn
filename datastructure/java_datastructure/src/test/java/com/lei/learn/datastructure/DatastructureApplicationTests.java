6828615f4829d0cfe08b3c18d9b198c917df8444
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-04-24 19:40:47 +0800

    [fix]

---
+++ datastructure/java_datastructure/src/test/java/com/lei/learn/datastructure/DatastructureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lei.learn.datastructure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatastructureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
