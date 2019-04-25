aeef3b58eef464193c80249fccce9117feee3b2d
Xu <lei.xu02@sap.com>
2019-04-25 18:18:42 +0800

    [fix]enable stack

--- datastructure/java_datastructure/src/main/java/com/lei/learn/datastructure/linearStructue/StackObject.java
+++ datastructure/java_datastructure/src/main/java/com/lei/learn/datastructure/linearStructue/StackObject.java
@@ -1,4 +1,38 @@
 package com.lei.learn.datastructure.linearStructue;
 
-public class StackObject {
+public class StackObject extends RuntimeException {
+  private String[] elements=new String[0];
+
+  public StackObject() {
+
+  }
+
+  //TODO push a new element
+  public void push(String element) {
+    String[] temp = new String[elements.length + 1];
+    for (int i = 0; i < elements.length; i++) {
+      temp[i] = elements[i];
+    }
+    temp[elements.length ] = element;
+    elements = temp;
+  }
+
+  //TODO pop the top element and remove it
+  public String pop() {
+    if (elements.length == 0) {
+      throw new RuntimeException("this is stack is empty");
+    }
+    String topElement = elements[elements.length - 1];
+    String[] temp = new String[elements.length - 1];
+    for (int i = 0; i < temp.length; i++) {
+      temp[i] = elements[i];
+    }
+    elements = temp;
+    return topElement;
+  }
+
+  //TODO Is this stack empty
+  public boolean isEmpty(){
+    return elements.length ==0;
+  }
 }
--- datastructure/java_datastructure/src/main/java/com/lei/learn/datastructure/linearStructue/StackTest.java
+++ datastructure/java_datastructure/src/main/java/com/lei/learn/datastructure/linearStructue/StackTest.java
@@ -2,6 +2,10 @@ package com.lei.learn.datastructure.linearStructue;
 
 public class StackTest {
   public static void main(String[] args) {
-
+    StackObject stackObject = new StackObject();
+    stackObject.push("a");
+    stackObject.push("b");
+    System.out.println(stackObject.pop());
+    System.out.println(stackObject.pop());
   }
 }
