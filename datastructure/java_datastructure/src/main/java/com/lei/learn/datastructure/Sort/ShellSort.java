dbbbf3a6d23474b91d654f76fd9332266cf5f0b3
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-05-02 22:37:35 +0800

    [add]shell sort ii

--- datastructure/java_datastructure/src/main/java/com/lei/learn/datastructure/Sort/ShellSort.java
+++ datastructure/java_datastructure/src/main/java/com/lei/learn/datastructure/Sort/ShellSort.java
@@ -11,10 +11,16 @@ public class ShellSort {
         System.out.println(Arrays.toString(tmp));
     }
 
+    /*
+    three loops
+    fhe first loop  until d ==1, d=d/2;
+    the second loop, loop the all array, by from the 0 one to the dth one;
+    the third loop, loop and sort the small array, from the left to the right one.
+     */
     private static int[] shellSort(int[] array) {
         for (int d = array.length / 2; d >= 1; d = d / 2)
-            for (int i = 0; i < d; i++)
-                for (int j = i - d / 2; j >= 0; j = j - d) {
+            for (int i = d; i < array.length; i++)
+                for (int j = i - d; j >= 0; j = j - d) {
                     if (array[j] > array[j + d]) {
                         int tmp = array[j];
                         array[j] = array[j + d];
