c06f24b93be8a71b3296e9ba21d2fe232264bdb2
Xu <lei.xu02@sap.com>
2019-05-03 17:11:14 +0800

    [init]enable spring boot learning

---
+++ sprintBoot/sprintBoot/src/main/java/com/lei/learn/sprintBoot/SprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.lei.learn.sprintBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootApplication.class, args);
+	}
+
+}
