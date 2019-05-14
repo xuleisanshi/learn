b4424c8bcb209273302e216a41c3e4b11b5d3c38
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-05-14 21:11:28 +0800

    [add]item286

--- leetcode/src/main/java/com/lei/learn/leetcode/Array/Item286.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Array/Item286.java
@@ -2,7 +2,7 @@ package com.lei.learn.leetcode.Array;
 
 import java.util.Arrays;
 
-public class Item283 {
+public class Item286 {
 
     /**
      * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
