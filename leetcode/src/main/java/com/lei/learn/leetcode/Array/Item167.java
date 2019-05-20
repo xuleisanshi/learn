3d16d0f2f0af5700d3c6bb8a4850876a9c6b1af4
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-05-20 23:19:41 +0800

    [add]item167

---
+++ leetcode/src/main/java/com/lei/learn/leetcode/Array/Item167.java
@@ -0,0 +1,66 @@
+package com.lei.learn.leetcode.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Item167 {
+
+    public static int[] twoSum(int[] numbers, int target) {
+
+        List<int[]> lists = new ArrayList<>();
+        int[] result = new int[2];
+
+        int index1;
+        int index2;
+        for (index1 = 0; index1 < numbers.length; index1++) {
+            for (index2 = index1 + 1; index2 < numbers.length; index2++)
+                if ((numbers[index1] + numbers[index2]) == target) {
+                    result[0] = index1 + 1;
+                    result[1] = index2 + 1;
+                    index2++;
+                    lists.add(result);
+                }
+        }
+        return lists.get(0);
+    }
+
+    public static int[] binaryMethod(int[] numbers, int target) {
+
+        int[] result = new int[2];
+        int index1 = 0;
+        for (; index1 < numbers.length; index1++) {
+            int tmp = target - numbers[index1];
+            int low = index1 + 1, high = numbers.length - 1;
+            int index2 = (low + high) / 2;
+            while (low <= high) {
+                if (tmp == numbers[index2]) {
+                    result[0] = index1 + 1;
+                    result[1] = index2 + 1;
+                    break;
+                } else if (tmp > numbers[index2]) {
+                    low = index2 + 1;
+                    index2 = (low + high) / 2;
+                } else {
+                    high = index2 - 1;
+                    index2 = (low + high) / 2;
+                }
+
+
+            }
+
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 11;
+
+
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+        System.out.println(Arrays.toString(binaryMethod(numbers, target)));
+
+    }
+}
--- leetcode/src/main/java/com/lei/learn/leetcode/Array/Item215.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Array/Item215.java
@@ -5,79 +5,79 @@ import java.util.PriorityQueue;
 
 public class Item215 {
 
-  /**
-   * Find the kth largest element in an unsorted array. Note that it is the kth largest element
-   * in the sorted order, not the kth distinct element.
-   * <p>
-   * Example 1:
-   * <p>
-   * Input: [3,2,1,5,6,4] and k = 2
-   * Output: 5
-   * Example 2:
-   * <p>
-   * Input: [3,2,3,1,2,4,5,5,6] and k = 4
-   * Output: 4
-   * Note:
-   * You may assume k is always valid, 1 ≤ k ≤ array's length.
-   */
+    /**
+     * Find the kth largest element in an unsorted array. Note that it is the kth largest element
+     * in the sorted order, not the kth distinct element.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: [3,2,1,5,6,4] and k = 2
+     * Output: 5
+     * Example 2:
+     * <p>
+     * Input: [3,2,3,1,2,4,5,5,6] and k = 4
+     * Output: 4
+     * Note:
+     * You may assume k is always valid, 1 ≤ k ≤ array's length.
+     */
 
-  public static int findKthLargest1(int[] nums, int k) {
-    for (int i = 0; i < nums.length && i < k; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        if (nums[i] < nums[j]) {
-          int tmp = nums[i];
-          nums[i] = nums[j];
-          nums[j] = tmp;
+    public static int findKthLargest1(int[] nums, int k) {
+        for (int i = 0; i < nums.length && i < k; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] < nums[j]) {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                }
+            }
         }
-      }
-    }
-    return nums[k - 1];
-  }
-
-  public static int findKthLargestByHeap(int[] nums, int k) {
-
-    PriorityQueue<Integer> integers = new PriorityQueue<>();
-    for (int i = 0; i < nums.length; i++) {
-      integers.offer(nums[i]);
+        return nums[k - 1];
     }
 
-  }
+    public static int findKthLargestByHeap(int[] nums, int k) {
 
-  public static int findKthLargest(int[] nums, int k) {
-
-    quickSort(nums, 0, nums.length - 1);
-
-    return nums[nums.length - k];
-  }
-
-  private static int[] quickSort(int[] array, int start, int end) {
-    int base = array[start];
-    int low = start;
-    int high = end;
-    if (start < end) {
-      while (low < high) {
-        while (base <= array[high] && high > low) {
-          high--;
+        PriorityQueue<Integer> integers = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            integers.offer(nums[i]);
         }
-        array[low] = array[high];
-        while (base >= array[low] && low < high) {
-          low++;
-        }
-        array[high] = array[low];
-      }
-
-      array[low] = base;
-      quickSort(array, start, low);
-      quickSort(array, low + 1, high);
+        return 0;
     }
-    return array;
-  }
 
-  public static void main(String[] args) {
-    int[] array = new int[]{3, 2, 1, 5, 6, 4};
-    int k = 2;
+    public static int findKthLargest(int[] nums, int k) {
+
+        quickSort(nums, 0, nums.length - 1);
+
+        return nums[nums.length - k];
+    }
+
+    private static int[] quickSort(int[] array, int start, int end) {
+        int base = array[start];
+        int low = start;
+        int high = end;
+        if (start < end) {
+            while (low < high) {
+                while (base <= array[high] && high > low) {
+                    high--;
+                }
+                array[low] = array[high];
+                while (base >= array[low] && low < high) {
+                    low++;
+                }
+                array[high] = array[low];
+            }
+
+            array[low] = base;
+            quickSort(array, start, low);
+            quickSort(array, low + 1, high);
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{3, 2, 1, 5, 6, 4};
+        int k = 2;
 //    System.out.println(findKthLargest1(array, k));
-    //    System.out.println(findKthLargest(array, k));
-    System.out.println(findKthLargestByHeap(array, k));
-  }
+        //    System.out.println(findKthLargest(array, k));
+        System.out.println(findKthLargestByHeap(array, k));
+    }
 }
