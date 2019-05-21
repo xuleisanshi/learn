3c4ba4c37b9e835f4f8801ea39646a69e45ec10d
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-05-21 22:37:49 +0800

    [add]item 125

---
+++ leetcode/src/main/java/com/lei/learn/leetcode/Array/Item125.java
@@ -0,0 +1,25 @@
+package com.lei.learn.leetcode.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item125 {
+
+
+    public static boolean isPalindrome(String s) {
+        boolean isPalindrome = false;
+        char[] chars = s.toCharArray();
+        int low = 0, high = chars.length - 1;
+        while (low<=high){
+            if (!Character.isLetterOrDigit())
+        }
+        return isPalindrome;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "0P";
+        System.out.println(isPalindrome(s));
+    }
+
+}
