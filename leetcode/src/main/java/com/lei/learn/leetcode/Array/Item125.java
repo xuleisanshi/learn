c96ed02eb9251cfcdd81dfaeb6b757b752edcc5a
Xu <lei.xu02@sap.com>
2019-05-22 17:52:34 +0800

    [add]item125 update

--- leetcode/src/main/java/com/lei/learn/leetcode/Array/Item125.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Array/Item125.java
@@ -1,25 +1,35 @@
 package com.lei.learn.leetcode.Array;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Item125 {
 
-
-    public static boolean isPalindrome(String s) {
-        boolean isPalindrome = false;
-        char[] chars = s.toCharArray();
-        int low = 0, high = chars.length - 1;
-        while (low<=high){
-            if (!Character.isLetterOrDigit())
-        }
-        return isPalindrome;
+  public static boolean isPalindrome(String s) {
+    boolean isPalindrome = false;
+    char[] chars = s.toCharArray();
+    int low = 0, high = chars.length - 1;
+    while (low < high) {
+      if (!Character.isLetterOrDigit(chars[low])) {
+        low++;
+        continue;
+      }
+      if (!Character.isLetterOrDigit(chars[high])) {
+        high--;
+        continue;
+      }
+      if (Character.toLowerCase(chars[low]) == Character.toLowerCase(chars[high])) {
+        low++;
+        high--;
+      } else {
+        break;
+      }
     }
-
-
-    public static void main(String[] args) {
-        String s = "0P";
-        System.out.println(isPalindrome(s));
+    if (low == high || (high + 1) == low) {
+      isPalindrome = true;
     }
+    return isPalindrome;
+  }
 
+  public static void main(String[] args) {
+    String s = "A man, a plan, a canal: Panama";
+    System.out.println(isPalindrome(s));
+  }
 }
