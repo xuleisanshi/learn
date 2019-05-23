53aa27e60201d1d969b56c6e6f7e7da9dd14da3e
Xu <lei.xu02@sap.com>
2019-05-23 18:04:30 +0800

    [add]item209

--- leetcode/src/main/java/com/lei/learn/leetcode/Array/Item209.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Array/Item209.java
@@ -3,10 +3,24 @@ package com.lei.learn.leetcode.Array;
 //two pointers
 
 /**
- *O(n^3),O(n),O(nlogn)
- *
+ * O(n^3),O(n),O(nlogn)
  */
 public class Item209 {
 
+  public static int minSubArrayLen(int s, int[] nums) {
+    int low, high;
+    for (int i = 0; i < nums.length - 1; i++) {
+      low = 0;
+      high = low + i;
+      int result = 0;
+      while (result < s) {
 
+      }
+    }
+    return result;
+  }
+
+  public static void main() {
+
+  }
 }
