2ea3cfdf4bb0f3c6e96adde804c0da7f0636143e
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-05-28 22:09:20 +0800

    [add]item438 and 209

--- leetcode/src/main/java/com/lei/learn/leetcode/Array/Item209.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Array/Item209.java
@@ -7,17 +7,48 @@ package com.lei.learn.leetcode.Array;
  */
 public class Item209 {
 
-  public static int minSubArrayLen(int s, int[] nums) {
-    int low, high;
-    int gap = 0;
+    public static int minSubArrayLen(int s, int[] nums) {
+        int low, high;
+        int gap = 0;
 
-    while (gap < nums.length) {
+        while (gap < nums.length) {
+        }
+
+        return 0;
     }
 
-    return 0;
-  }
 
-  public static void main() {
+    private static int solveNLogN(int s, int[] nums) {
+        int[] sums = new int[nums.length + 1];
+        for (int i = 1; i < sums.length; i++) sums[i] = sums[i - 1] + nums[i - 1];
+        int minLen = Integer.MAX_VALUE;
+        for (int i = 0; i < sums.length; i++) {
+            int end = binarySearch(i + 1, sums.length - 1, sums[i] + s, sums);
+            if (end == sums.length) break;
+            if (end - i < minLen) minLen = end - i;
+        }
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
 
-  }
+    private static int binarySearch(int lo, int hi, int key, int[] sums) {
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (sums[mid] >= key) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+
+    public static void main(String[] args) {
+
+        int s = 7;
+        int[] nums = new int[]{2, 3, 1, 2, 4, 3};
+        System.out.println(solveNLogN(s, nums));
+
+
+    }
 }
