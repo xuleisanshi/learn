ffb8308b559e7cf2afdb0bf87d2c58bce7693b44
Xu <lei.xu02@sap.com>
2019-05-28 09:15:32 +0800

    [add]item209

--- leetcode/src/main/java/com/lei/learn/leetcode/Array/Item209.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Array/Item209.java
@@ -7,17 +7,46 @@ package com.lei.learn.leetcode.Array;
  */
 public class Item209 {
 
+  //  O(n^3)
   public static int minSubArrayLen(int s, int[] nums) {
-    int low, high;
     int gap = 0;
-
-    while (gap < nums.length) {
+    for (; gap < nums.length; gap++) {
+      for (int i = 0; i < nums.length - gap; i++) {
+        int sum = 0;
+        for (int j = i; j <= i + gap; j++) {
+          sum = sum + nums[j];
+        }
+        if (sum >= s) {
+          return (gap + 1) > Integer.MIN_VALUE ? gap + 1 : Integer.MIN_VALUE;
+        }
+      }
     }
-
     return 0;
   }
 
-  public static void main() {
+  //O(N)
+  public static int minSubArrayLenOn(int s, int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
 
+    int result = Integer.MAX_VALUE, left = 0;
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum = sum + nums[i];
+      while (sum >= s && left < nums.length) {
+        result = result > (i - left) ? i + 1 - left : result;
+        sum = sum - nums[left];
+        left++;
+      }
+    }
+    return result == Integer.MAX_VALUE ? 0 : result;
+  }
+
+  public static void main(String[] args) {
+
+    int s = 0;
+    int[] nums = new int[]{1, 2, 3};
+    System.out.println(minSubArrayLenOn(s, nums));
   }
 }
