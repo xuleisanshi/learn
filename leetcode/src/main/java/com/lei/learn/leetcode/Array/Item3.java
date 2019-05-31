aa04fe34fb1cba91a06de607cb9c531ab5114dd4
Xu <lei.xu02@sap.com>
2019-05-31 09:45:14 +0800

    [add]item3

--- leetcode/src/main/java/com/lei/learn/leetcode/Array/Item3.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Array/Item3.java
@@ -4,4 +4,22 @@ package com.lei.learn.leetcode.Array;
  * two pointer
  */
 public class Item3 {
+
+  public static int lengthOfLongestSubstring(String s) {
+
+    int result = 1;
+    if (s.length() == 0) {
+      return 0;
+    }
+
+    char[] chars = s.toCharArray();
+    int right = 0;
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    String s = "abcabcbb";
+    System.out.println(lengthOfLongestSubstring(s));
+  }
 }
---
+++ leetcode/src/main/java/com/lei/learn/leetcode/CollectionLearn/SetAndMap.java
@@ -0,0 +1,13 @@
+package com.lei.learn.leetcode.CollectionLearn;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * learn set and map
+ * part of Collection.
+ */
+public class SetAndMap {
+
+  Set<String> strings = new HashSet<>();
+}
