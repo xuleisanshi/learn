e832fb6874e8c8760d9666909222a776ff0d242b
Xu <lei.xu02@sap.com>
2019-05-31 10:26:57 +0800

    [add]rename set and map

--- leetcode/src/main/java/com/lei/learn/leetcode/CollectionLearn/SetAndMap.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/CollectionLearn/SetAndMap.java
@@ -1,13 +1,32 @@
 package com.lei.learn.leetcode.CollectionLearn;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * learn set and map
- * part of Collection.
- */
 public class SetAndMap {
 
-  Set<String> strings = new HashSet<>();
+  public static void main(String[] args) {
+
+    //        Set<Integer> ints = new HashSet<>();
+    //
+    //        ints.add(1);
+    //        System.out.println(ints.contains(1));
+    //        System.out.println(ints.size());
+    //        System.out.println(ints.remove(1));
+
+    Map<Integer, String> integerIntegerMap = new HashMap<>();
+    System.out.println(integerIntegerMap.put(1, "a"));
+    System.out.println(integerIntegerMap.put(2, "b"));
+    System.out.println(integerIntegerMap.get(1) + 1);
+    System.out.println(integerIntegerMap.entrySet());
+    System.out.println(((HashMap<Integer, String>) integerIntegerMap).clone());
+    System.out.println(integerIntegerMap.containsValue("a"));
+    System.out.println(integerIntegerMap.keySet());
+    System.out.println(integerIntegerMap.values());
+
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(1, 1);
+    map.put(1, map.get(1) + 1);
+    System.out.println(map.get(1));
+  }
 }
