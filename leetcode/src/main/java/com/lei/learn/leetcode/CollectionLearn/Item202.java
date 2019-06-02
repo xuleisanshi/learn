a74e7700c1c411cb913e720f297ce925eb4ef944
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-06-02 13:10:58 +0800

    [add]item202 item 242

--- leetcode/src/main/java/com/lei/learn/leetcode/CollectionLearn/Item202.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/CollectionLearn/Item202.java
@@ -1,4 +1,46 @@
 package com.lei.learn.leetcode.CollectionLearn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Item202 {
+
+
+    //TODO 快慢指针
+
+
+    public static boolean isHappy(int n) {
+        boolean result = true;
+        int sum = n;
+        List<Integer> list = new ArrayList<>();
+        list.add(sum);
+        while (sum != 1) {
+            sum = getSum(sum);
+            if (list.contains(sum)) {
+                result = false;
+                break;
+            }
+            list.add(sum);
+        }
+        return result;
+    }
+
+    public static int getSum(int n) {
+        int number = n;
+        int sum = 0;
+        while (number > 0) {
+            sum = sum + (number % 10) * (number % 10);
+            number = number / 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        System.out.println(isHappy(n));
+
+    }
 }
--- leetcode/src/main/java/com/lei/learn/leetcode/CollectionLearn/Item242.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/CollectionLearn/Item242.java
@@ -1,4 +1,66 @@
 package com.lei.learn.leetcode.CollectionLearn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Item242 {
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
+        boolean result = true;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < chars.length; i++) {
+            map.put(chars[i], map.getOrDefault(chars[i], 0) + 1);
+        }
+        for (int j = 0; j < chart.length; j++) {
+            if (!map.containsKey(chart[j]) || map.get(chart[j]) < 1) {
+                result = false;
+                break;
+            } else {
+                map.put(chart[j], map.get(chart[j]) - 1);
+            }
+        }
+        return result;
+    }
+
+
+    public static boolean isAnagram1(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < chars.length; i++) {
+            count[chars[i] - 'a']++;
+            count[chart[i] - 'a']--;
+        }
+
+        boolean result = true;
+
+        for (int i : count) {
+            if (i != 0) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "a";
+        String t = "a";
+        System.out.println(isAnagram1(s, t));
+
+    }
+
+
 }
