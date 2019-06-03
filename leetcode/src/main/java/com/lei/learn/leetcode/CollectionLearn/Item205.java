daaf0564fa5a8357a42377dae8e3b28378127a88
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-06-03 20:25:52 +0800

    [add]item205 451

--- leetcode/src/main/java/com/lei/learn/leetcode/CollectionLearn/Item205.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/CollectionLearn/Item205.java
@@ -1,8 +1,49 @@
 package com.lei.learn.leetcode.CollectionLearn;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Item205 {
-    public boolean isIsomorphic(String s, String t) {
+
+
+    //TODO O(n)
+    public static boolean isIsomorphicOn(String s, String t) {
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
         return false;
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
+        Map<Character, Character> map = new HashMap<>();
+        boolean result = true;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(chars[i]) != null) {
+                if (map.get(chars[i]) != chart[i]) {
+                    result = false;
+                    break;
+                }
+            } else {
+                if (!map.containsValue(chart[i])) map.put(chars[i], chart[i]);
+                else return false;
+            }
+
+        }
+        return result;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        String s = "ab";
+        String t = "cc";
+
+        System.out.println(isIsomorphic(s, t));
 
     }
 
