6af0cbd6b8d537155e720d8b94ca4fed4a05251a
Xu <lei.xu02@sap.com>
2019-06-12 18:11:14 +0800

    [add]item49

--- leetcode/src/main/java/com/lei/learn/leetcode/KSum/Item49.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/KSum/Item49.java
@@ -10,14 +10,8 @@ public class Item49 {
 
     List<List<String>> lists = new ArrayList<>();
 
-    Map<Integer, Map<Character, Integer>> map = new HashMap<>();
-    for (int i = 0; i < strs.length; i++) {
-      Map<Character, Integer> tmp = new HashMap<>();
-      for (int j = 0; j < strs[i].length(); j++) {
-        tmp.put(strs[i].charAt(j), tmp.getOrDefault(strs[i].charAt(j), 0) + 1);
-      }
-      map.put(i, tmp);
-    }
+    Map<Integer, String> map = new HashMap<>();
+
     return null;
   }
 }
