d25c177fea4c37d36d37c958d9c4c7bb84ce7de1
Lei Xu <leixu@LeideMacBook-Pro.local>
2019-06-13 22:22:55 +0800

    [add]item149

--- leetcode/src/main/java/com/lei/learn/leetcode/KSum/Item149.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/KSum/Item149.java
@@ -5,30 +5,59 @@ import java.util.Map;
 
 public class Item149 {
 
-  //not correct, should be continued!~
-  public static int maxPoints(int[][] points) {
-    int max = 0;
-    if (points.length <= 2) {
-      return points.length;
+    //not correct, should be continued!~
+    public static int maxPoints(int[][] points) {
+        if (points == null) return 0;
+        if (points.length <= 2) return points.length;
+
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        int result = 0;
+        for (int i = 0; i < points.length; i++) {
+            map.clear();
+            int overlap = 0, max = 0;
+            for (int j = i + 1; j < points.length; j++) {
+                int x = points[j][0] - points[i][0];
+                int y = points[j][1] - points[i][1];
+                if (x == 0 && y == 0) {
+                    overlap++;
+                    continue;
+                }
+                int gcd = generateGCD(x, y);
+                if (gcd != 0) {
+                    x /= gcd;
+                    y /= gcd;
+                }
+
+                if (map.containsKey(x)) {
+                    if (map.get(x).containsKey(y)) {
+                        map.get(x).put(y, map.get(x).get(y) + 1);
+                    } else {
+                        map.get(x).put(y, 1);
+                    }
+                } else {
+                    Map<Integer, Integer> m = new HashMap<>();
+                    m.put(y, 1);
+                    map.put(x, m);
+                }
+                max = Math.max(max, map.get(x).get(y));
+            }
+            result = Math.max(result, max + overlap + 1);
+        }
+        return result;
     }
 
-    Map<Double, Integer> map = new HashMap<>();
+    private static int generateGCD(int a, int b) {
 
-    for (int i = 0; i < points.length - 1; i++) {
-      for (int j = i + 1; j < points.length; j++) {
-        int dx = points[j][0] - points[i][0];
-        int dy = points[j][1] - points[i][1];
-      }
+        if (b == 0) return a;
+        else return generateGCD(b, a % b);
+
     }
 
-    return max;
-  }
-
-  public static void main(String[] args) {
-    //{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}
-    //{0, 0}, {1, 1}, {0, 0}
-    //{1,1},{1,1},{1,1}
-    int[][] points = new int[][]{{0, 0}, {1, 1}, {0, 0}};
-    System.out.println(maxPoints(points));
-  }
+    public static void main(String[] args) {
+        //{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}
+        //{0, 0}, {1, 1}, {0, 0}
+        //{1,1},{1,1},{1,1}
+        int[][] points = new int[][]{{0, 0}, {1, 1}, {0, 0}};
+        System.out.println(maxPoints(points));
+    }
 }
