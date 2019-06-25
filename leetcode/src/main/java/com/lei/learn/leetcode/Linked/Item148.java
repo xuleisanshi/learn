60e9c78e2449c9a13c64043453ee7d5d7d8fc20e
runnerstone <xulei.99@hotmail.com>
2019-06-25 21:15:58 +0800

    [add]item148

---
+++ leetcode/src/main/java/com/lei/learn/leetcode/Linked/Item148.java
@@ -0,0 +1,15 @@
+package com.lei.learn.leetcode.Linked;
+
+public class Item148 {
+
+    public static ListNode sortList(ListNode head) {
+
+        //TODO 难度较大，以后再做
+
+            return head;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
