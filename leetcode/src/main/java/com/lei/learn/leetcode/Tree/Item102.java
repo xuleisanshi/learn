784b9d86a3109eebff11e355cfa1ecfcf8547140
runnerstone <xulei.99@hotmail.com>
2019-06-29 16:19:32 +0800

    :sparkles: item 102 with queue level order traversal

--- leetcode/src/main/java/com/lei/learn/leetcode/Tree/Item102.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Tree/Item102.java
@@ -1,37 +1,68 @@
 package com.lei.learn.leetcode.Tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Item102 {
-  public static class TreeNode {
+    public static class TreeNode {
 
-    int val;
-    TreeNode left;
-    TreeNode right;
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-    TreeNode(int x) {
-      val = x;
-    }
-  }
-
-  public List<List<Integer>> levelOrder(TreeNode root) {
-    List<List<Integer>> result = new ArrayList<>();
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-
-    while (!queue.isEmpty()) {
-      List<Integer> tmpList = new ArrayList<>();
-      TreeNode tmpNode = ((LinkedList<TreeNode>) queue).pop();
-      tmpList.add(tmpNode.val);
+        TreeNode(int x) {
+            val = x;
+        }
     }
 
-    return result;
-  }
+    public static List<List<Integer>> levelOrder(TreeNode root) {
 
-  public static void main(String[] args) {
+        List<List<Integer>> result = new ArrayList<>();
 
-  }
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> tmpList = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode tmp = ((LinkedList<TreeNode>) queue).pop();
+                tmpList.add(tmp.val);
+                if (tmp.left != null) {
+                    queue.add(tmp.left);
+                }
+                if (tmp.right != null) {
+                    queue.add(tmp.right);
+                }
+
+            }
+            result.add(tmpList);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node8 = new TreeNode(8);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        node4.left = node8;
+
+        System.out.println(levelOrder(node1).toString());
+    }
 }
