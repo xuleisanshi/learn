bf5501284bbe967368f4443b2607d01664c7934f
runnerstone <xulei.99@hotmail.com>
2019-07-01 23:01:08 +0800

    :art: add item222 without code

---
+++ leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item222.java
@@ -0,0 +1,29 @@
+package com.lei.learn.leetcode.Recursion;
+
+public class Item222 {
+    //Definition for a binary tree node.
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public int countNodes(TreeNode root) {
+
+
+        return 1;
+
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
