1072c11bd68e1f43762853041351caa3105eb3b0
runnerstone <xulei.99@hotmail.com>
2019-07-01 20:18:50 +0800

    :art: use priority queue to merge k sorted lists

--- leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item23.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item23.java
@@ -1,24 +1,50 @@
 package com.lei.learn.leetcode.Recursion;
 
+import java.util.PriorityQueue;
+
 public class Item23 {
 
-  // Definition for singly-linked list.
-  public static class ListNode {
-    int val;
-    ListNode next;
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
 
-    ListNode(int x) {
-      val = x;
+        ListNode(int x) {
+            val = x;
+        }
     }
-  }
 
-  public static ListNode mergeKLists(ListNode[] lists) {
+    public static ListNode mergeKLists(ListNode[] lists) {
 
-    ListNode listNode;
-    return null;
-  }
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
 
-  public static void main(String[] args) {
+        for (int i = 0; i < lists.length; i++) {
+            ListNode tmp = lists[i];
+            while (tmp != null) {
+                priorityQueue.add(tmp.val);
+                tmp = tmp.next;
+            }
+        }
+        ListNode preNode = new ListNode(0);
+        ListNode current = preNode;
 
-  }
+        while (!priorityQueue.isEmpty()) {
+            current.next = new ListNode(priorityQueue.remove());
+            current = current.next;
+        }
+        return preNode.next;
+    }
+
+    public static void main(String[] args) {
+
+
+        ListNode node1 = new ListNode(0);
+        ListNode node2 = new ListNode(9);
+        ListNode node3 = new ListNode(1);
+
+        node1.next = new ListNode(6);
+
+        ListNode[] lists = new ListNode[]{node1, node2, node3};
+        System.out.println(mergeKLists(lists).toString());
+    }
 }
