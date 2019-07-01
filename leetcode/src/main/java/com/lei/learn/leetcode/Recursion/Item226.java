cb97d73e454c2d429eb65ce4c9ed7b6c26ace60c
runnerstone <xulei.99@hotmail.com>
2019-07-01 21:46:40 +0800

    :art: item100 in item226 class compare two binary trees by using recursiong

--- leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item226.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item226.java
@@ -33,29 +33,55 @@ public class Item226 {
 
     }
 
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
+
+
+    }
+
     public static void main(String[] args) {
+//        TreeNode node1 = new TreeNode(1);
+//        TreeNode node2 = new TreeNode(2);
+//        TreeNode node3 = new TreeNode(3);
+//        TreeNode node4 = new TreeNode(4);
+//        TreeNode node5 = new TreeNode(5);
+//        TreeNode node6 = new TreeNode(6);
+//        TreeNode node7 = new TreeNode(7);
+//        TreeNode node8 = new TreeNode(8);
+//        TreeNode node9 = new TreeNode(9);
+//        TreeNode node10 = new TreeNode(10);
+//        TreeNode node11 = new TreeNode(11);
+//
+//        node1.left = node2;
+//        node1.right = node3;
+//        node2.left = node4;
+//        node2.right = node5;
+//        node3.left = node6;
+//        node3.right = node7;
+//        node4.left = node8;
+//        node4.right = node9;
+//        node6.left = node10;
+//        node6.right = node11;
+
+
         TreeNode node1 = new TreeNode(1);
         TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node4 = new TreeNode(4);
-        TreeNode node5 = new TreeNode(5);
-        TreeNode node6 = new TreeNode(6);
-        TreeNode node7 = new TreeNode(7);
-        TreeNode node8 = new TreeNode(8);
-        TreeNode node9 = new TreeNode(9);
-        TreeNode node10 = new TreeNode(10);
-        TreeNode node11 = new TreeNode(11);
+        TreeNode node3 = new TreeNode(1);
+
 
         node1.left = node2;
-        node1.right = node3;
-        node2.left = node4;
-        node2.right = node5;
-        node3.left = node6;
-        node3.right = node7;
-        node4.left = node8;
-        node4.right = node9;
-        node6.left = node10;
-        node6.right = node11;
+
+        node3.right = node2;
+
+        System.out.println(isSameTree(node1, node3));
+
+
+        System.out.println(true || false);
+
     }
 
 
