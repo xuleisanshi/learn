ad92dcb21b8dd1cd74e695918b46c9946c9492ef
Xu <lei.xu02@sap.com>
2019-07-01 18:10:15 +0800

    :sparkles: item23

---
+++ leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item23.java
@@ -0,0 +1,24 @@
+package com.lei.learn.leetcode.Recursion;
+
+public class Item23 {
+
+  // Definition for singly-linked list.
+  public static class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+    }
+  }
+
+  public static ListNode mergeKLists(ListNode[] lists) {
+
+    ListNode listNode;
+    return null;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
