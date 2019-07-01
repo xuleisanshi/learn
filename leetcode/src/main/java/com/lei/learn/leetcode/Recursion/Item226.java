6878da34bfe77aac67b02c099ea32a8c2ce20108
runnerstone <xulei.99@hotmail.com>
2019-07-01 21:18:53 +0800

    :sparkles: use recursion to revert a binary tree

---
+++ leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item226.java
@@ -0,0 +1,62 @@
+package com.lei.learn.leetcode.Recursion;
+
+public class Item226 {
+
+    //Definition for a binary tree node.
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.left == null && root.left == null) {
+            return root;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        root.right = invertTree(root.right);
+        root.left = invertTree(root.left);
+        return root;
+
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node8 = new TreeNode(8);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node10 = new TreeNode(10);
+        TreeNode node11 = new TreeNode(11);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        node4.left = node8;
+        node4.right = node9;
+        node6.left = node10;
+        node6.right = node11;
+    }
+
+
+}
