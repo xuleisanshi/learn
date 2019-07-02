52bd14823fa8c29ab380cce9d7008989840968e9
runnerstone <xulei.99@hotmail.com>
2019-07-02 20:30:06 +0800

    :art: get the count of one complete binary tree

--- leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item222.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item222.java
@@ -13,16 +13,56 @@ public class Item222 {
     }
 
 
-    public int countNodes(TreeNode root) {
+    public static int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
 
-
-        return 1;
+        int leftNode = countNodes(root.left);
+        int rightNode = countNodes(root.right);
+        return leftNode + rightNode + 1;
 
     }
 
 
+    public static int countNodesByCompleteTree(TreeNode root) {
+
+        int leftDepth = getLeftDeep(root);
+        int rightDepth = getRightDeep(root);
+        if (leftDepth == rightDepth) {
+            return (1 << leftDepth) - 1;
+        } else
+            return 1 + countNodesByCompleteTree(root.left) + countNodesByCompleteTree(root.right);
+    }
+
+
+    public static int getLeftDeep(TreeNode root) {
+        int level = 0;
+        while (root != null) {
+            root = root.left;
+            level++;
+        }
+        return level;
+    }
+
+    public static int getRightDeep(TreeNode root) {
+        int level = 0;
+        while (root != null) {
+            root = root.right;
+            level++;
+        }
+        return level;
+    }
+
+    public static int mostLeftLevel(TreeNode root, int level) {
+        while (root != null) {
+            level++;
+            root = root.left;
+        }
+        return level - 1;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 
 
