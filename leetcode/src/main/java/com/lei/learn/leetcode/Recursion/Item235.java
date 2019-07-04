9634ca04247442654c6df509a740f66063a0df25
runnerstone <xulei.99@hotmail.com>
2019-07-04 21:56:45 +0800

    :art: find acl of two node in a binary serarch tree

---
+++ leetcode/src/main/java/com/lei/learn/leetcode/Recursion/Item235.java
@@ -0,0 +1,23 @@
+package com.lei.learn.leetcode.Recursion;
+
+public class Item235 {
+
+
+    //Definition for a binary tree node.
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        else if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        else return root;
+    }
+}
