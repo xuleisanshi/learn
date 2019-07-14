52ec1d5f65e7ad0f1d9207b144ca9f2c2859a46c
runnerstone <10296021+runnerstone@users.noreply.github.com>
2019-07-14 13:50:13 +0800

    :sprarkles: add item200 by copying other's answer

---
+++ leetcode/src/main/java/com/lei/learn/leetcode/Recursion/BackTrack/Item130.java
@@ -0,0 +1,7 @@
+package com.lei.learn.leetcode.Recursion.BackTrack;
+
+public class Item130 {
+    public void solve(char[][] board) {
+
+    }
+}
---
+++ leetcode/src/main/java/com/lei/learn/leetcode/Recursion/BackTrack/Item200.java
@@ -0,0 +1,32 @@
+package com.lei.learn.leetcode.Recursion.BackTrack;
+
+public class Item200 {
+
+    private int m, n;
+
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        n = grid.length;
+        if (n == 0) return 0;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    ++count;
+                }
+        }
+        return count;
+    }
+
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
+    }
+
+
+}
