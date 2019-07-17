4b8e7e4dcda3e4b5ba708f4561cce56cbff3bc47
runnerstone <10296021+runnerstone@users.noreply.github.com>
2019-07-17 21:45:25 +0800

    :sparkles: add item 64

--- leetcode/src/main/java/com/lei/learn/leetcode/DynamicProgramming/Item64.java
+++ leetcode/src/main/java/com/lei/learn/leetcode/DynamicProgramming/Item64.java
@@ -1,10 +1,27 @@
 package com.lei.learn.leetcode.DynamicProgramming;
 
+import cucumber.api.java.it.Ma;
+
 public class Item64 {
 
     public int minPathSum(int[][] grid) {
 
-        return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] mem = new int[m][n];
+        assert m > 0 && n > 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) mem[i][j] = grid[i][j];
+                else if (i == 0 && j > 0) mem[i][j] = mem[i][j - 1] + grid[i][j];
+                else if (j == 0 && i > 0) mem[i][j] = mem[i - 1][j] + grid[i][j];
+                else mem[i][j] = Math.min(mem[i][j - 1], mem[i - 1][j]) + grid[i][j];
+            }
+        }
+
+
+        return mem[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
