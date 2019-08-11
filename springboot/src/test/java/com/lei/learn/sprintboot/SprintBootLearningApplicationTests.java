b2907f5af2c2964f9c7985b53ca3086743979945
runnerstone <10296021+runnerstone@users.noreply.github.com>
2019-08-11 16:09:21 +0800

    :art:config pom

--- springboot/src/test/java/com/lei/learn/sprintboot/SprintBootLearningApplicationTests.java
+++ springboot/src/test/java/com/lei/learn/sprintboot/SprintBootLearningApplicationTests.java
@@ -1,9 +1,7 @@
 package com.lei.learn.sprintboot;
 
-import com.lei.learn.sprintboot.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,13 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes=SprintBootLearningApplication.class)
 public class SprintBootLearningApplicationTests {
 
-
-	@Autowired
-	Person person;
-
 	@Test
 	public void contextLoads() {
-		System.out.println(person);
 	}
 
 }
