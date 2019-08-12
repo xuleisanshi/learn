4e26df6bbffce4e88f8f0e8bdc2f07104940cebc
runnerstone <10296021+runnerstone@users.noreply.github.com>
2019-08-12 21:24:52 +0800

    :art: actuator

--- springboot/src/main/java/com/lei/learn/sprintBoot/repo/ReaderRepository.java
+++ springboot/src/main/java/com/lei/learn/sprintBoot/repo/ReaderRepository.java
@@ -1,9 +1,9 @@
-package com.lei.learn.sprintBoot.repo;
-
-import com.lei.learn.sprintBoot.bean.Reader;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ReaderRepository extends JpaRepository<Reader, String> {
-
-}
+//package com.lei.learn.sprintBoot.repo;
+//
+//import com.lei.learn.sprintBoot.bean.Reader;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//
+//public interface ReaderRepository extends JpaRepository<Reader, String> {
+//
+//}
