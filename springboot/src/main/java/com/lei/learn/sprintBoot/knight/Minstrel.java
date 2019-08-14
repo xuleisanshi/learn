6aa4aa5631316dc132c7ca01e354236b133a576f
Xu <lei.xu02@sap.com>
2019-08-14 16:43:05 +0800

    :art: add data source failed, need more invest

---
+++ springboot/src/main/java/com/lei/learn/sprintBoot/knight/Minstrel.java
@@ -0,0 +1,16 @@
+package com.lei.learn.sprintBoot.knight;
+
+public class Minstrel {
+
+  public Minstrel() {
+
+  }
+
+  public void singBeforeQuest() {
+    System.out.println("Fa la la, the knight is so brave!");
+  }
+
+  public void singAfterQuest() {
+    System.out.println("Tee hee hee, the brave knight did embark on a quest");
+  }
+}
