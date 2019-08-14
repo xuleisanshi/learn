e22a045775472d7840a22ee5a8eb8c3684e97e2a
runnerstone <10296021+runnerstone@users.noreply.github.com>
2019-08-14 23:08:23 +0800

    :art: config postresql and yml

--- springboot/src/main/java/com/lei/learn/sprintBoot/bean/Dog.java
+++ springboot/src/main/java/com/lei/learn/sprintBoot/bean/Dog.java
@@ -1,6 +1,16 @@
 package com.lei.learn.sprintBoot.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Dog {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private String id;
   private String name;
   private Integer age;
 
--- springboot/src/main/java/com/lei/learn/sprintBoot/config/DataSourceConfig.java
+++ springboot/src/main/java/com/lei/learn/sprintBoot/config/DataSourceConfig.java
@@ -2,18 +2,14 @@ package com.lei.learn.sprintBoot.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
-@ConfigurationProperties(prefix = "spring.datasource.hikari")
 public class DataSourceConfig extends HikariConfig {
 
-  @Bean
-  public DataSource dataSource() {
-    return new HikariDataSource(this);
-  }
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(this);
+    }
 }
