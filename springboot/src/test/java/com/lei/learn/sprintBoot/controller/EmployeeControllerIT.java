e9e88154df3ee30d97ff5cb63824e55909387324
runnerstone <10296021+runnerstone@users.noreply.github.com>
2019-08-19 22:43:10 +0800

    :art: add base it class to load contexgt

--- springboot/src/test/java/com/lei/learn/sprintBoot/controller/EmployeeControllerIT.java
+++ springboot/src/test/java/com/lei/learn/sprintBoot/controller/EmployeeControllerIT.java
@@ -4,28 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.lei.learn.sprintBoot.bean.Employee;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-public class EmployeeControllerIT {
+
+public class EmployeeControllerIT extends BaseIT {
     @Autowired
     MockMvc mockMvc;
-
 
     @Test
     public void saveEmployee_whenGiveEmployee_thenStatus200() throws Exception {
