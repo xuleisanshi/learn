8cf770d6a558b29393b4c7ff15975d7d50647416
Xu <lei.xu02@sap.com>
2019-08-20 13:30:21 +0800

    :art: test

---
+++ springboot/src/test/java/com/lei/learn/sprintBoot/testframework/learnJsonTest.java
@@ -0,0 +1,10 @@
+package com.lei.learn.sprintBoot.testframework;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@JsonTest
+public class JsonTest {
+  
+}
