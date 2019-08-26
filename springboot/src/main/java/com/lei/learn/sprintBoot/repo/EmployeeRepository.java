e5510cc86e0b302df66ba89fe0d1a02d5fe392f7
Xu <lei.xu02@sap.com>
2019-08-26 09:12:08 +0800

    :art: change application properitest

--- springboot/src/main/java/com/lei/learn/sprintBoot/repo/EmployeeRepository.java
+++ springboot/src/main/java/com/lei/learn/sprintBoot/repo/EmployeeRepository.java
@@ -3,7 +3,7 @@ package com.lei.learn.sprintBoot.repo;
 import com.lei.learn.sprintBoot.bean.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
   Employee findByName(String name);
 }
