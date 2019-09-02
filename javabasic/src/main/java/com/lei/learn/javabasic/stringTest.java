9dbefdab420882471d7a9385b7913ef51fa94a46
runnerstone <10296021+runnerstone@users.noreply.github.com>
2019-09-02 21:16:43 +0800

    :art: add project java baisc

---
+++ javabasic/src/main/java/com/lei/learn/javabasic/stringTest.java
@@ -0,0 +1,23 @@
+package com.lei.learn.javabasic;
+
+public class stringTest {
+
+    public static void main(String[] args) {
+
+        char a = 'a';
+        String b = "a";
+
+
+        System.out.println("=======char========");
+        System.out.println(a);
+        System.out.println(a + 0);
+
+        System.out.println("=======String========");
+        System.out.println(b);
+        System.out.println(b.hashCode());
+        System.out.println(b.getBytes());
+        System.out.println(b.intern());
+    }
+
+
+}
