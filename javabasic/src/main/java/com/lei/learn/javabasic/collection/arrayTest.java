2d20750531a8ea9e02169a1d77e04fb3600de3e1
Xu <lei.xu02@sap.com>
2019-09-04 17:41:10 +0800

    :art: update arraytest

--- javabasic/src/main/java/com/lei/learn/javabasic/collection/arrayTest.java
+++ javabasic/src/main/java/com/lei/learn/javabasic/collection/arrayTest.java
@@ -2,8 +2,24 @@ package com.lei.learn.javabasic.collection;
 
 public class arrayTest {
 
+  private String a;
+  private int b;
+
   public static void main(String[] args) {
-    Thread thread = new Thread();
-    thread.run();
+    String a = "asd";
+    String b = new String("asd");
+
+    Double d = 1.0;
+    int i1 = 12;
+    int i2 = 13;
+    int i3 = 12;
+    int i4 = 13;
+
+    int i5 = add(i1, i2);
+  }
+
+  public static int add(int a, int b) {
+    int r = a + b;
+    return r;
   }
 }
