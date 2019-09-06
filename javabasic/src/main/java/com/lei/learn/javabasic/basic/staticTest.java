8ba4f041f4ede78580c17455d0d59631cd6161b4
runnerstone <10296021+runnerstone@users.noreply.github.com>
2019-09-06 23:17:47 +0800

    :art: static inner class

--- javabasic/src/main/java/com/lei/learn/javabasic/basic/staticTest.java
+++ javabasic/src/main/java/com/lei/learn/javabasic/basic/staticTest.java
@@ -1,10 +1,15 @@
 package com.lei.learn.javabasic.basic;
 
+import static java.lang.Float.min;
+import static java.lang.Math.max;
+
 public class staticTest {
 
     private static void Print() {
         System.out.println("Print()");
     }
+
+    private String name;
 
     public static void main(String[] args) {
 
@@ -13,6 +18,29 @@ public class staticTest {
          * 此处由于Print是静态方法， 静态方法是跟着class走的，class加载的时候就为print分配了内存空间
          * 而非静态方法则是跟着对象的，当对象实例化的时候才分配内存空间，因此此处输出Print()
          */
-        ((staticTest)null).Print();
+        ((staticTest) null).Print();
+        inner inn = new inner();
+        inn.method();
+        /**
+         * 静态导包
+         */
+        min(1, 3);
+        max(1, 3);
+    }
+
+
+    /**
+     * 静态内部类
+     */
+
+    public static class inner {
+        public String a;
+
+        public void method() {
+            System.out.println("静态内部类方法");
+            staticTest.Print();
+        }
+
+
     }
 }
